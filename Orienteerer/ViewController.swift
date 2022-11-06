df8395541c98209faa8b0f89bfcd132b7be32ee4
kl-awn <80085373+kl-awn@users.noreply.github.com>
2022-11-06 14:33:24 -0800

    Initial Commit

---
+++ Orienteerer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Orienteerer
+//
+//  Created by Tom Yuan on 11/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
