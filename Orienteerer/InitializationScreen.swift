c7bf870cfdb55797aa9937b51c904cf52e90cd83
Kugahoo <zyuan2250@outlook.com>
2022-12-29 13:48:44 -0800

    Added initialization screen

---
+++ Orienteerer/InitializationScreen.swift
@@ -0,0 +1,15 @@
+//
+//  InitializationScreen.swift
+//  Orienteerer
+//
+//  Created by Kevin Yuan on 12/29/22.
+//
+
+import UIKit
+
+class InitializationScreen: NSObject {
+    @IBAction func AcceptTerms(_ sender: Any) {
+        let eigthVC = self.storyboard?.instantiateViewController(withIdentifier: "HomeScreen") as! HomeScreen
+                self.navigationController?.pushViewController(eigthVC, animated: true)
+    }
+}
