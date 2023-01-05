c59e26a21d71b0d768b936853602d90f35895b13
Kugahoo <zyuan2250@outlook.com>
2023-01-04 18:07:41 -0800

    added popup

--- Orienteerer/SetupAssistant.swift
+++ Orienteerer/SetupAssistant.swift
@@ -7,28 +7,24 @@
 
 import UIKit
 import HealthKit
-class SetupAssistant: NSObject {
-    var healthStore:HKHealthStore?
-    func gainAuthorization(){
-        let allTypes = Set([HKObjectType.workoutType(),
-                            HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
-                            HKObjectType.quantityType(forIdentifier: .distanceCycling)!,
-                            HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
-                            HKObjectType.quantityType(forIdentifier: .heartRate)!])
 
-        healthStore!.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
-            if !success {
-                // Handle the error here.
-            }
+let healthStore = HKHealthStore()
+
+    
+let allTypes = Set([HKObjectType.workoutType(),
+                    HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+                    HKObjectType.quantityType(forIdentifier: .distanceCycling)!,
+                    HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+                    HKObjectType.quantityType(forIdentifier: .heartRate)!])
+func authorize(){
+    healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+        if !success {
+            authorize()
+            // Handle the error here.
         }
     }
-    func setupHealthKit() {
-        
-        if HKHealthStore.isHealthDataAvailable(){
-            
-            healthStore = HKHealthStore()
-        }
-         
-    }
-
 }
+
+        
+
+
