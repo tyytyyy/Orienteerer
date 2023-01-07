f63d9902c226c077c69e6fbb3313c59da8e18d50
Kugahoo <zyuan2250@outlook.com>
2023-01-06 19:56:46 -0800

    worked with HealthKit

--- Orienteerer/HomeScreen.swift
+++ Orienteerer/HomeScreen.swift
@@ -4,19 +4,27 @@
 //
 //  Created by Tom Yuan on 11/6/22.
 //
-
+ 
 import UIKit
 import HealthKit
-
-
-class HomeScreen: UIViewController {
-
+ 
+ 
+class HomeScreen: UIViewController{
+    private var healthStore:HealthStore? = HealthStore()
+    
+  
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        authorize()
+        if let healthStore = healthStore{
+            healthStore.requestAuthorization{success in
+                
+            }
+        }
         // Do any additional setup after loading the view.
         
-
+ 
     }
     
     @IBAction func nextScreen(_ sender: Any) {
@@ -25,4 +33,4 @@ class HomeScreen: UIViewController {
     }
     
 }
-
+ 
--- Orienteerer/SetupAssistant.swift
+++ Orienteerer/SetupAssistant.swift
@@ -1,30 +1,48 @@
+
 //
 //  SetupAssistant.swift
 //  Orienteerer
 //
 //  Created by Kevin Yuan on 1/3/23.
 //
-
+ 
 import UIKit
 import HealthKit
-
-let healthStore = HKHealthStore()
-
+class HealthStore{
+    var healthStore:HKHealthStore? //creates variable healthStore of type HKHealthStore
     
-let allTypes = Set([HKObjectType.workoutType(),
-                    HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
-                    HKObjectType.quantityType(forIdentifier: .distanceCycling)!,
-                    HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
-                    HKObjectType.quantityType(forIdentifier: .heartRate)!])
-func authorize(){
-    healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
-        if !success {
-            authorize()
-            // Handle the error here.
+    init(){ //initializes healthStore as HKHealthStore() if HKHealthStore is available
+        if HKHealthStore.isHealthDataAvailable(){
+            healthStore = HKHealthStore()
         }
     }
-}
-
+    
+    func requestAuthorization(completion: @escaping (Bool) -> Void){ //requests authorization once, need to change what is needed
+        let allTypes = Set([HKObjectType.workoutType(),
+                            HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+                            HKObjectType.quantityType(forIdentifier: .distanceCycling)!,
+                            HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+                            HKObjectType.quantityType(forIdentifier: .heartRate)!])
+        guard let healthStore = self.healthStore else { return completion(false)}
+        healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+            completion(success)
+        }
         
-
-
+    }
+    
+    func getHeartRate(){
+        guard let sampleType = HKObjectType.quantityType(forIdentifier: .heartRate) else{
+            return
+        }
+        let startDate = Calendar.current.date(byAdding: .month, value: -1, to: Date())
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: Date(), options:.strictEndDate)
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierStartDate, ascending: false)
+        let query = HKSampleQuery(sampleType: sampleType, predicate: predicate, limit: Int(HKObjectQueryNoLimit), sortDescriptors: [sortDescriptor]) {(sample, result, error) in
+            guard error == nil else{
+                return
+            }
+        }
+        healthStore?.execute(query)
+    }
+ 
+}
