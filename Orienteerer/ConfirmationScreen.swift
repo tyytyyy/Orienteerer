649c54da011192ee2546b5d5104b885b5759bc9a
kl-awn <80085373+kl-awn@users.noreply.github.com>
2023-03-12 18:15:54 -0700

    Added data processing to scanned data

--- Orienteerer/ConfirmationScreen.swift
+++ Orienteerer/ConfirmationScreen.swift
@@ -34,6 +34,8 @@ class ConfirmationScreen: UIViewController {
     
     var image1: UIImage!
     var cgImage: CGImage!
+    var timelist: [UITextField] = []
+    var inttimelist: [Int] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(label)
@@ -71,9 +73,10 @@ class ConfirmationScreen: UIViewController {
             // Return the string of the top VNRecognizedText instance.
             return observation.topCandidates(1).first?.string
         }
-        var y = 100
-        var x = 50
+        var y = 170
+        var x = 70
         var count = 0
+        let height:Int = Int(UIScreen.main.bounds.height)
         for i in recognizedStrings{
             if(i.contains("(")&&i.contains(")")){
                 let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 24))
@@ -83,28 +86,34 @@ class ConfirmationScreen: UIViewController {
                 label.font = UIFont(name:"Futura-Medium", size:24)
                 self.view.addSubview(label)
                 y = y+50
-                if(y>800){
-                    y = 100
+                
+                if(y>height-100){
+                    y = 170
                     x+=200
                 }
                 count+=1
             }
         }
-        x = 150
-        y = 100
+        x = 160
+        y = 170
         var count2 = -1
         for i in recognizedStrings{
             if(i.contains(":")){
                 if(count2>0&&count2%2==1){
-                    let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 24))
+                    let label = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 24))
+                    timelist.append(label)
+                    inttimelist.append(converttoseconds(hours: i))
                     label.center = CGPoint(x: x, y: y)
                     label.textAlignment = .center
                     label.text = i
+                    label.returnKeyType = .done
                     label.font = UIFont(name:"Futura-Medium", size:24)
+                    label.addTarget(self, action: #selector(ConfirmationScreen.textFieldDidChange(_:)), for: .editingChanged)
                     self.view.addSubview(label)
+                    
                     y = y+50
-                    if(y>800){
-                        y = 100
+                    if(y>height-100){
+                        y = 170
                         x+=200
                     }
                     print(count2)
@@ -113,16 +122,34 @@ class ConfirmationScreen: UIViewController {
                 
             }
         }
-        
     }
     
+    func converttoseconds(hours: String?) -> Int!{
+        if (hours==nil){
+            return 0;
+        }
+        let timesplit = hours!.split(separator: ":")
+        var timeseconds = 0
+        var multiplyer = 1
+        for n in timesplit.reversed(){
+            timeseconds+=Int.parse(from: String(n))*multiplyer
+            multiplyer*=60
+        }
+        return timeseconds
+    }
     
+    @objc func textFieldDidChange(_ label: UITextField) {
+        let index = timelist.firstIndex(of: label)!
+        print("UWU")
+        inttimelist[index] = converttoseconds(hours: label.text!)
+    }
     
     
     @IBAction func RetakePicture(_ sender: Any) {
         let fifthVC = self.storyboard?.instantiateViewController(withIdentifier: "ScanScreen") as! ScanScreen
                 self.navigationController?.pushViewController(fifthVC, animated: true)
     }
+    
     @IBAction func ConfirmPicture(_ sender: Any) {
         let sixthVC = self.storyboard?.instantiateViewController(withIdentifier: "EndScreen") as! EndScreen
                 self.navigationController?.pushViewController(sixthVC, animated: true)
@@ -130,4 +157,13 @@ class ConfirmationScreen: UIViewController {
     
     
     
+}
+extension Int {
+    static func parse(from string: String) -> Int {
+        var k = Int(string.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        if(k==nil){
+            return 0;
+        }
+        return k!
+    }
 }
